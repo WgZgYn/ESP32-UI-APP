f5b229edbd51eab9565bc522d4fcbf97e0bcff29
wzy <2049562063@qq.com>
2024-11-04 17:06:54 +0800

    fix

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -5,9 +5,13 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#define DEBUG_SRT() do {
+#define DEBUG_END() } while(0)
+
+
 namespace utils {
     template<typename T>
-    T abs_diff(T& a, T& b) {
+    T abs_diff(const T& a, const T& b) {
         return a > b ? a - b : b - a;
     }
 } // utils
