bb362fcf812f023296626a2fbdb6ee860d24e5eb
wzy <2049562063@qq.com>
2024-12-05 20:24:17 +0800

    init service

--- src/service/service.cpp
+++ src/service/service.cpp
@@ -1,108 +1,174 @@
 #include "service.h"
 #include <ArduinoJson.h>
+#include <hal/hal.h>
 #include <utils/utils.h>
 
+constexpr int AIR_CONDITION_OFFSET = 1;
 
-const char *Service::openLight() {
-    light = true;
-    brightness = 255;
-    analogWrite(LED_BUILTIN, 255);
-    return "open";
-}
+const String Service::ID{ESP.getEfuseMac() + AIR_CONDITION_OFFSET};
 
-const char *Service::closeLight() {
-    light = false;
-    brightness = 0;
-    analogWrite(LED_BUILTIN, 0);
-    return "close";
-}
-
-const char *Service::switchLight() {
-    return light ? closeLight() : openLight();
-}
-
-void Service::setBrightness(const uint8_t val) {
-    float scaledVal = val * 2.56;
-
-    // 确保 scaledVal 在 0 到 255 之间
-    if (scaledVal > 255) scaledVal = 255;
-    if (scaledVal < 0) scaledVal = 0;
-
-    // 将浮点数转换为 uint8_t
-    const auto targetBrightness = static_cast<uint8_t>(scaledVal);
-
-
-    Serial.print("from ");
-    Serial.print(brightness);
-    Serial.print(" to ");
-    Serial.println(targetBrightness);
-
-    // avoid divide 0
-    if (brightness == targetBrightness) {
-        return;
+const char *modeStr(const int mode) {
+    switch (mode) {
+        case 0:
+            return "Heating";
+        case 1:
+            return "Refrigeration";
+        case 2:
+            return "Dehumidify";
+        case 3:
+            return "AirSupply";
+        default:
+            return "";
     }
+}
 
-    const int step = targetBrightness > brightness ? 1 : -1;
-
-
-    const int stepDelay = 500 / utils::abs_diff(targetBrightness, brightness);
-
-    // 使用循环更新亮度
-    while (brightness != targetBrightness) {
-        brightness += step; // 增加或减少亮度
-        analogWrite(LED_BUILTIN, brightness);
-        delay(stepDelay);
-        yield();
+const char *windStr(const int wind) {
+    switch (wind) {
+        case 0:
+            return "Low";
+        case 1:
+            return "Mid";
+        case 2:
+            return "High";
+        default:
+            return "";
     }
+}
 
-    Serial.println(brightness);
+StaticJsonDocument<512> newJsonDocument() {
+    StaticJsonDocument<512> doc;
+    doc["efuse_mac"] = Service::ID;
+    doc["model_id"] = Service::model_id;
+    doc["model_name"] = Service::model_name;
+    return doc;
 }
 
 void Service::init() {
-    pinMode(LED_BUILTIN, OUTPUT); // 使用蓝色LED管脚
-    pinMode(RESET_BUTTON_PIN, INPUT_PULLUP); // 按钮连接到GPIO0，低电平触发
     Serial.println("SERVICE initialized");
 }
 
-bool Service::callback(const char *cmd, StaticJsonDocument<128> &doc) {
-    if (strcmp(cmd, "open") == 0) {
-        doc["payload"] = openLight();
-        return true;
+void Service::reportStatus() const {
+    StaticJsonDocument<512> doc(newJsonDocument());
+    doc["type"] = "status";
+    const JsonObject status = doc.createNestedObject("payload"); // Otherwise create a new one
+
+    status["电源"] = power;
+    status["温度"] = temperature;
+    status["风速"] = windSpeed;
+    status["模式"] = mode;
+
+    String res;
+    serializeJson(doc, res);
+    mqtt::publish(mqtt::MQTT_PUBLISH_STATUS, 1, res.c_str());
+}
+
+void Service::setPower(bool power) {
+    StaticJsonDocument<512> doc(newJsonDocument());
+    doc["type"] = "event";
+    const JsonObject payload = doc.createNestedObject("payload"); // Otherwise create a new one
+
+    this->power = power;
+    payload["电源"] = this->power;
+
+    String res;
+    serializeJson(doc, res);
+    mqtt::publish(mqtt::MQTT_PUBLISH_EVENTS, 2, res.c_str());
+}
+
+void Service::setMode(int mode) {
+    StaticJsonDocument<512> doc(newJsonDocument());
+    doc["type"] = "event";
+    const JsonObject payload = doc.createNestedObject("payload"); // Otherwise create a new one
+
+    if (mode > 3 || mode < 0) {
+        return;
     }
-    if (strcmp(cmd, "close") == 0) {
-        doc["payload"] = closeLight();
-        return true;
+    this->mode = mode;
+    payload["模式"] = this->mode;
+
+    String res;
+    serializeJson(doc, res);
+    mqtt::publish(mqtt::MQTT_PUBLISH_EVENTS, 2, res.c_str());
+}
+
+void Service::setWindSpeed(int speed) {
+    StaticJsonDocument<512> doc(newJsonDocument());
+    doc["type"] = "event";
+    const JsonObject payload = doc.createNestedObject("payload"); // Otherwise create a new one
+
+    if (speed > 2 || speed < 0) {
+        return;
     }
-    if (strcmp(cmd, "switch") == 0) {
-        doc["payload"] = switchLight();
-        return true;
+
+    windSpeed = speed;
+    payload["风速"] = windSpeed;
+
+    String res;
+    serializeJson(doc, res);
+    mqtt::publish(mqtt::MQTT_PUBLISH_EVENTS, 2, res.c_str());
+}
+
+void Service::setTemperature(float temp) {
+    StaticJsonDocument<512> doc(newJsonDocument());
+    doc["type"] = "event";
+    const JsonObject payload = doc.createNestedObject("payload"); // Otherwise create a new one
+
+    if (temp > maxTemp || temp < minTemp) {
+        return;
     }
-    if (strncmp(cmd, "light=", 6) == 0) {
-        const char *val = cmd + 6;
-        char *endptr;
-        const unsigned intValue = strtoul(val, &endptr, 10);
-        if (endptr != val) {
-            setBrightness(intValue);
-            doc["payload"] = cmd;
-            return true;
+    temp = static_cast<float>(static_cast<int>(temp * 2)) / 2.0f;
+    temperature = temp;
+    payload["温度"] = temperature;
+
+    String res;
+    serializeJson(doc, res);
+    mqtt::publish(mqtt::MQTT_PUBLISH_EVENTS, 2, res.c_str());
+}
+
+
+void Service::callback(const StaticJsonDocument<512> &message) {
+    const char *serviceName = message["service_name"];
+    if (strcmp("status", serviceName) != 0) {
+        if (strcmp("power-on", serviceName) == 0) {
+            setPower(true);
+        } else if (strcmp("power-off", serviceName) == 0) {
+            setPower(false);
+        } else if (strcmp("temperature", serviceName) == 0) {
+            const float temp = message["payload"];
+            setTemperature(temp);
+        } else if (strcmp("wind", serviceName) == 0) {
+            const int windSpeed = message["payload"];
+            setWindSpeed(windSpeed);
+        } else if (strcmp("mode", serviceName) == 0) {
+            const int mode = message["payload"];
+            setMode(mode);
         }
-        if (strcmp(val, "true") == 0) {
-            doc["payload"] = openLight();
-            return true;
-        }
-        if (strcmp(val, "false") == 0) {
-            doc["payload"] = closeLight();
-            return true;
-        }
-        return false;
     }
-    if (strcmp(cmd, "status") == 0) {
-        // Change the type to status
-        doc["type"] = "status";
-        JsonObject status = doc.createNestedObject("payload");
-        status["灯开关"] = light ? "true" : "false";
-        status["灯亮度"] = brightness;
-        return true;
-    }
-    return false;
+    reportStatus();
+}
+
+void ServiceUI::onInit() {
+}
+
+bool ServiceUI::onOpen() {
+    return true;
+}
+
+void ServiceUI::onLoop() {
+    HAL::cancelInfo();
+    const String power = String("power: ") + String(service.power);
+    HAL::printInfo(power);
+    HAL::delay(50);
+    const String wind = String("wind: ") + windStr(service.windSpeed);
+    HAL::printInfo(wind);
+    HAL::delay(50);
+    const String mode = String("mode: ") + modeStr(service.mode);
+    HAL::printInfo(mode);
+    HAL::delay(50);
+    const String temp = String("temp: ") + String(service.temperature);
+    HAL::printInfo(temp);
+    HAL::delay(50);
+}
+
+void ServiceUI::onExit() {
 }
--- src/service/service.h
+++ src/service/service.h
@@ -8,34 +8,74 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <ArduinoJson/Document/StaticJsonDocument.hpp>
+#include <mqtt/mqtt.h>
 
 #define LED_BUILTIN 2
 #define RESET_BUTTON_PIN 0 // 按钮引脚，使用GPIO 0
 
 
 class Service {
-    bool light = false;
-    uint8_t brightness = 0;
-
-    const char* openLight();
-
-    const char* closeLight();
-
-    const char* switchLight();
-
-    void setBrightness(uint8_t val);
-
 public:
+    enum WindSpeed {
+        Low,
+        Mid,
+        High
+    };
+
+    enum Mode {
+        Heating,
+        Refrigeration,
+        Dehumidify,
+        AirSupply,
+    };
+
+    static constexpr uint8_t model_id = 9;
+    static constexpr auto model_name = "ESP-air-condition";
+    static const String ID;
+
+    static constexpr float minTemp = 18;
+    static constexpr float maxTemp = 30;
+
+    float temperature = 26.0;
+    int windSpeed = 0;
+    int mode = 0;
+    bool power = true;
+
+    void reportStatus() const;
+
+    void setPower(bool power);
+    void setMode(int mode);
+    void setWindSpeed(int speed);
+    void setTemperature(float temp);
+
     static void init();
 
-    bool callback(const char *cmd, ArduinoJson::StaticJsonDocument<128>& doc);
+    void callback(const ArduinoJson::StaticJsonDocument<512> &message);
 
-    static Service& getInstance() {
+    static Service &getInstance() {
         static Service service;
         return service;
     }
 };
 
+
+class ServiceUI : public astra::ui::LifecycleItem {
+    Service &service = Service::getInstance();
+
+public:
+    void onInit() override;
+
+    bool onOpen() override;
+
+    void onLoop() override;
+
+    void onExit() override;
+
+    static ServiceUI &getInstance() {
+        static ServiceUI serviceUI;
+        return serviceUI;
+    }
+};
 
 #define SERVICE_H
 #endif
