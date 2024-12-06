2cab30807223c07d89623f53610e442064eb3935
wzy <2049562063@qq.com>
2024-12-06 11:20:52 +0800

    test ok

--- src/service/service.cpp
+++ src/service/service.cpp
@@ -79,7 +79,7 @@ void Service::reportStatus() const {
     const JsonObject status = doc.createNestedObject("payload"); // Otherwise create a new one
 
     status["灯开关"] = brightness > 0;
-    status["灯亮度"] = static_cast<int>(brightness / 2.55); // TODO:
+    status["灯亮度"] = static_cast<int>(brightness / 2.55);
 
     String res;
     serializeJson(doc, res);
